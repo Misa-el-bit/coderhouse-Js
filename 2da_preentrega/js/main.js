441b6f6d78534b638759f3427a0dfb31ef918f95
Misa-El <mmancillaglz07@gmail.com>
2023-02-21 15:09:04 -0600

    Segunda preentrega

---
+++ 2da_preentrega/js/main.js
@@ -0,0 +1,31 @@
+
+let numberOfProducts = prompt("Ingrese el numero de productos a guardar")
+
+const productList = (numberOfProducts) =>{
+    let productsArray = []
+
+    for (i=0; i< numberOfProducts; i++){
+        let productName = prompt("Ingresa el nombre del producto")
+        let productPrice = parseInt(prompt("Ingresa el precio"))
+
+        class Producto {
+            constructor(nombre, precio){
+                this.nombre = nombre; 
+                this.precio = precio;
+            }     
+        }
+        const myProduct = new Producto(productName, productPrice)
+        console.log(myProduct)
+
+        productsArray.push(myProduct)
+        console.log(productsArray)
+    }
+
+    productsArray.forEach( product =>{
+        let productName = product.nombre 
+        let price = product.precio * 1.16
+        alert(`El precio con iva de ${productName} es: $ ${ price }`)
+    })
+}
+productList(numberOfProducts)
+
